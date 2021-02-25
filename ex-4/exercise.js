d02c315155a8f53873f55b4e59c537523d127db4
gando diallo <gandoesprit@gmail.com>
2021-02-25 15:24:00 +0100

    solution to exercice 4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,14 @@
-// Example
-window.alert("Hello World!");
+var myName = prompt("Put your name");
+window.alert("Hello " + myName);
+
+var yourDay = prompt("How your day was good/bad");
+
+
+if(yourDay === 'good' || yourDay === 'GOOD')
+{
+    window.alert("I'm happy for you champ");
+}
+else
+{
+    window.alert("That's too bad, tomorrow it's a new day");
+}
