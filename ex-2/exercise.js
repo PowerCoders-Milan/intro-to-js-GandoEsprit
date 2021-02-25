803ab3fa5be234448c4cfbacf573b1a98c048d1c
gando diallo <gandoesprit@gmail.com>
2021-02-25 23:28:10 +0100

    Begin work to extra on exercice 2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -4,29 +4,39 @@ var yearNow = 2021;
 var age = yearNow - yearWasBorn;
 window.alert("I will be either " + age + " or " + (age + 1) + " in " + yearNow);
 
-/*var dayWasBorn = 7;
-var monthWasBorn = 5;
+yearWasBorn = new Date(1989, 5, 7);
+yearNow = Date.now();
 
-var futurDay = 10;
-var futurMonth = 6;
-
-var dayAge , dayMonth;
+var month = 0, day = 0;
 
 
-if(dayWasBorn === futurDay)
-{
-    dayAge = 0;
-    if(monthWasBorn === futurMonth)
-    {
-        dayMonth = 0;
+if(yearWasBorn.getMont() === yearNow.getMont()){
+    if(yearWasBorn.getDate() === yearNow.getDate()){
         age = yearNow - yearWasBorn;
     }
-    else if(monthWasBorn > futurMont)
-    {
-        age = yearNow - yearWasBorn - 1;
+    else if(yearWasBorn.getDate() > yearNow.getDate()){
+        day = yearWasBorn.getDay() - yearNow.getDay();
+        age = yearNow.getFullYear() - yearWasBorn.getFullYear();
     }
     else
     {
-
+        day = yearNow.getDate() - yearWasBorn.getDate();
+        month = 11;
+        age = yearNow.getFullYear() - yearWasBorn.getFullYear() - 1;
     }
-}*/
+}
+else if(yearWasBorn.getMonth() > yearNow.getMonth()){
+    if(yearWasBorn.getDate() === yearNow.getDate()){
+        mont = yearWasBorn.getMonth() - yearNow.getMonth();
+        age = yearNow.getFullYear() - yearWasBorn.getFullYear() - 1;
+    }
+    else if(yearWasBorn.getDate() > yearNow.getDate()){
+        day = yearWasBorn.getDate() - yearNow.getDate();
+        mont = yearWasBorn.getMonth() - yearNow.getMonth();
+        age = yearNow.getFullYear() - yearWasBorn.getFullYear() - 1;
+    }
+    else{
+        
+    }
+}
+window.alert(yearWasBorn.getFullYear() + ' ' + yearWasBorn.getDate() + ' ' + yearWasBorn.getMonth());
