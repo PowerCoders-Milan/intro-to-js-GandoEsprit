0581a52677980fdad7b13b90e86cdcf509641b54
gando diallo <gandoesprit@gmail.com>
2021-02-25 12:13:08 +0100

    Solution for exercice1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,10 @@
 // Example
-window.alert("Hello World!");
+var numberOfChildren = 2;
+var partenerName = "Fatoumata";
+var geographicLocation = 'Rimini';
+var jobTitle = 'Programmer';
+window.alert("You will be a " +  jobTitle + " in " + geographicLocation +
+ ", and married to " +  partenerName + " with " + numberOfChildren + " kids.");
+
+ console.log("You will be a " +  jobTitle + " in " + geographicLocation +
+ ", and married to " +  partenerName + " with " + numberOfChildren + " kids.");
