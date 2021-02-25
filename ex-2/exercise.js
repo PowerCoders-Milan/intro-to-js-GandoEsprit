dcbb009d474d893a913c961f6ab70fcf4821f834
gando diallo <gandoesprit@gmail.com>
2021-02-25 15:06:42 +0100

    Solution to exercise 3

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -3,3 +3,30 @@ var yearWasBorn = 1989;
 var yearNow = 2021;
 var age = yearNow - yearWasBorn;
 window.alert("I will be either " + age + " or " + (age + 1) + " in " + yearNow);
+
+/*var dayWasBorn = 7;
+var monthWasBorn = 5;
+
+var futurDay = 10;
+var futurMonth = 6;
+
+var dayAge , dayMonth;
+
+
+if(dayWasBorn === futurDay)
+{
+    dayAge = 0;
+    if(monthWasBorn === futurMonth)
+    {
+        dayMonth = 0;
+        age = yearNow - yearWasBorn;
+    }
+    else if(monthWasBorn > futurMont)
+    {
+        age = yearNow - yearWasBorn - 1;
+    }
+    else
+    {
+
+    }
+}*/
--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,16 @@
 // Example
-window.alert("Hello World!");
+var number1 = 10;
+var number2 = 10;
+
+if(number1 > number2)
+{
+    window.alert("The greater number of " + number1 + " and " + number2 + " is " + number1 + ".");
+}
+else if(number1 < number2)
+{
+    window.alert("The greater number of " + number1 + " and " + number2 + " is " + number2 + ".");
+}
+else
+{
+    window.alert("The numbers are the same.");
+}
