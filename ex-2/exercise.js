ed8ed103af1877d11e3b1114bd4cb3c02c72260f
gando diallo <gandoesprit@gmail.com>
2021-02-25 12:58:44 +0100

    Solution to exercice2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,5 @@
 // Example
-window.alert("Hello World!");
+var yearWasBorn = 1989;
+var yearNow = 2021;
+var age = yearNow - yearWasBorn;
+window.alert("I will be either " + age + " or " + (age + 1) + " in " + yearNow);
